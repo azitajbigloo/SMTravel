dc475403bd04543e561ec8f5e2da2b016557ae27
Hang(LEO) Gong <47484740+hgong11@users.noreply.github.com>
2020-03-27 21:54:18 -0400

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,14 @@
+package simModel;
+
+public class constants
+{
+  /* Constants */
+    // Example: protected final static double realConstant = 8.0;
+    // Define constants as static
+    
+    final static int GOLD = 0;//Identifier for gold operators.
+    final static int SILVER = 1;//Identifier for silver operators.
+    final static int REGULAR = 2;//Identifier for regular operators.
+    final static double EWTDuration = 8.0;//wait time duration for the voice in the phone to estimate the expected wait time
+    
+}
