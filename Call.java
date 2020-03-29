9f3450fa200e01a03ea9f1557890172e95b1bd04
azitajbigloo <60991637+azitajbigloo@users.noreply.github.com>
2020-03-29 00:00:21 -0400

    Update Call.java

--- Call.java
+++ Call.java
@@ -1,15 +1,24 @@
-package simModel;
-
-// ZIBO MENG
+package SMTravelSimulation;
+/*
+ * Call Entities Class 
+ */
 public class Call {
-
-	int startWaitTime;
-	String uCuType;
-	String uCaType;
+    int uCuType;        // Type of Customer 
+    int uCaType;        // Type of Call
+    double startWaitTime;  // Time the Call arrives 
 	
-	public Call(int time, String cutype, String catype){
-		startWaitTime=time;
-		uCuType=cutype;
-		uCaType=catype;
+    //Constructor     
+    /***** I don't think this class needs a constructor??? ***/
+	public Call() {
+		//int time, String cutype, String catype){
+		//startWaitTime = time;
+		//uCuType = cutype;
+		//uCaType = catype;
 	}
+	/*
+	 * public String toString() { return ("Call: uType = " +
+	 * Constants.CALLTYPES[uCuType] + ", uSubject = " +
+	 * Constants.CALL_SUBJECTS[uCaType] + ", uToleratedWaitTime = " +
+	 * uToleratedWaitTime + ", startWaitTime = " + startWaitTime);
+	}*/
 }
