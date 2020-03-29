9ff15704f43d07c5d857c33da87ad04bd8a9e6aa
azitajbigloo <60991637+azitajbigloo@users.noreply.github.com>
2020-03-28 23:55:52 -0400

    Update RegularCallArrival.java

--- RegularCallArrival.java
+++ RegularCallArrival.java
@@ -1,3 +1,4 @@
+package SMTravelSimulation;
 import simulationModelling.ScheduledAction;
 public class RegularCallArrival extends ScheduledAction{
 	
@@ -5,22 +6,26 @@ public class RegularCallArrival extends ScheduledAction{
 
 	    RegularCallArrival(SMTravel model) { this.model = model; }
 
-	   
+	    @Override
 	    public double timeSequence()
 	    {
 	        return(model.rvp.duCallReg());
 	    }
 
-	  
+	    @Override
 	    protected void actionEvent() {
-	        // Arrival Action Sequence SCS
-	    	icCall = model.spDerive(Call);
+	      
+			// Arrival Action Sequence SCS
+	    	Call icCall = model.spDerive(icCall);
+	    	// Call icCall = new Call();
 	    	icCall.uCuType = Constants.REGULAR;
 	    	icCall.uCaType = model.rvp.uCaType();	 
 	    	model.udp.UpdateNumArrivalsOtput(icCall.uCuType);
 	    	if(TrunkLine.n < (Trunkline.numLines - TrunkLine.numReserved)) {
 	    		model.spInsertGrp( rgTrunkLine, icCall);
-	    		model.spStartSequel(EstimateWaitTime, icCall);
+	    		//model.rgTrunkLine.add(icCall);
+	    		model.spStartSequel(EstimateWaitTime(icCall)); // deleted icCall from the parameters, but it should be in the CM
+	    		
 	    	}
 	    	else {
 	    		model.udp.UpdateNumBusySOutput(icCall.uCuType);
