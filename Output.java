4a99b4c4da95f4fe15c69863c1573023a33474aa
azitajbigloo <60991637+azitajbigloo@users.noreply.github.com>
2020-03-28 23:56:27 -0400

    Update Output.java

--- Output.java
+++ Output.java
@@ -1,39 +1,42 @@
-package simModel;
-
-class Output 
-{
-//	SMTravel model;
-
-//      ZIBO MENG	
+package SMTravelSimulation;
+/*
+ * Outputs
+ */
+public class Output {
 	
-//	protected Output(ModelName md) {model = md; }
-    // Use OutputSequence class to define Trajectory and Sample Sequences
-    // Trajectory Sequences
+		SMTravel model;
 
-    // Sample Sequences
+//	      ZIBO MENG	
+		
+		protected Output(SMTravel md) {model = md; }
+	    // Use OutputSequence class to define Trajectory and Sample Sequences
+	    // Trajectory Sequences
 
-    // DSOVs available in the OutputSequence objects
-    // If seperate methods required to process Trajectory or Sample
-    // Sequences - add them here
+	    // Sample Sequences
 
-    // SSOVs
-	int numGoldCalls;
-	int num90SecGoldCalls;
-	int numSilverCalls;
-	int num180SecSilverCalls;
-	int numRegularCalls;
-	int num900SecRegularCalls;
-	
-	int numBusyCrdHCalls;
-	int numCardHArrivalCalls;
-	int numBusyRegularCalls;
-	int numRegularArrivalCalls;
-	
-	double perc900SecRegularCalls=numRegularCalls/num900SecRegularCalls;
-	double perc180SecSilverCalls=num180SecSilverCalls/numSilverCalls;
-	double perc90SecGoldCalls=num90SecGoldCalls/numGoldCalls;
-	double percBusyCrdHCalls=numBusyCrdHCalls/(numGoldCalls+numSilverCalls);
-	double percBusyRegularCalls=numBusyRegularCalls/numRegularArrivalCalls;
-	
-	//remeber to design a print method in simulation part
+	    // DSOVs available in the OutputSequence objects
+	    // If separate methods required to process Trajectory or Sample
+	    // Sequences - add them here
+
+	    // SSOVs
+		int numGoldCalls; 			//The number of gold calls that call the SM Travel center.
+		int num90SecGoldCalls; 		//The number of gold calls that exceeded the 90 second wait time.
+		int numSilverCalls; 		//The number of silver calls that enter the SM Travel system.
+		int num180SecSilverCalls; 	//The number of silver calls that exceeded the 180 second wait time.
+		int numRegularCalls; 		//The number of regular calls that enter the SM Travel system.
+		int num900SecRegularCalls;  //The number of regular calls that exceeded the 900 second wait time.
+		
+		int numBusyCrdHCalls;  		//The number of gold/silver calls that receive a busy signal.
+		int numCardHArrivalCalls;  	//The number of card hold calls that arrived at the very start of the system.
+		int numBusyRegularCalls; 	//The number of regular calls that receive a busy signal.
+		int numRegularArrivalCalls; //The number of regular calls that arrived at the very start of the system.
+		
+		double perc900SecRegularCalls = numRegularCalls / num900SecRegularCalls;  	//The percentage of regular calls that exceeded the 900 second wait time.
+		double perc180SecSilverCalls = num180SecSilverCalls / numSilverCalls; 		//The percentage of silver calls that exceeded the 180 second wait time.
+		double perc90SecGoldCalls = num90SecGoldCalls / numGoldCalls; 				//The percentage of gold calls that exceeded the 90 second wait time.
+		double percBusyCrdHCalls = numBusyCrdHCalls / (numGoldCalls+numSilverCalls);//The percentage of card hold calls that receive busy signal.
+		double percBusyRegularCalls = numBusyRegularCalls / numRegularArrivalCalls; //The percentage of regular calls that receive a busy signal.
+		
+		//remember to design a print method in simulation part
+
 }
