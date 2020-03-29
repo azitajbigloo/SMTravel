0b2cae25635c382110cf5464c898d527ea62a291
azitajbigloo <60991637+azitajbigloo@users.noreply.github.com>
2020-03-28 23:59:16 -0400

    Update CardHolderCallArrival.java

--- CardHolderCallArrival.java
+++ CardHolderCallArrival.java
@@ -1,3 +1,4 @@
+package SMTravelSimulation;
 import simulationModelling.ScheduledAction;
 public class CardHolderCallArrival extends ScheduledAction {
 	
@@ -8,25 +9,27 @@ public class CardHolderCallArrival extends ScheduledAction {
 	        this.model = model;
 	    }
 
-	  
+	    @Override
 	    public double timeSequence() {
 	        return (model.rvp.duCallCrd());
 	    }
 
-	   
+	    @Override
 	    protected void actionEvent() {
 	    	 // Arrival Action Sequence SCS
-	    	icCall = model.spDerive(Call);
+	    	Call icCall = model.spDerive(icCall);
 	    	icCall.uCaType = model.rvp.uCaType();
-	    	model.udp.UpdateNumArrivalsOutput(Constants.SILVER);
+	    	model.udp.UpdateNumArrivalsOutput(icCall.uCuType);
 	    	if(TrunkLine.n < TrunkLine.numLines) {
 	    		model.spInsertGrp(rgTrunkLine, icCall);
-	    		model.spStartSequel(InputMemberNumber, icCall);
+	    		//model.rgTrunkLine.add(icCall);
+	    		model.spStartSequel(InputMemberNumber(icCall));// deleted icCall from the parameters, but it should be in the CM
 	    		
 	    	}
 	    	else {
 	    		model.udp.UpdateNumBusySOutput(icCall.uCuType);
 	    		model.spLeave(icCall);
+	    		
 	    	}
 	    }
 	}
