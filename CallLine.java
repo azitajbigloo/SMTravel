e72c9c8b7eed7679995c0dc99496f7efb43a1638
azitajbigloo <60991637+azitajbigloo@users.noreply.github.com>
2020-03-28 23:59:49 -0400

    Update and rename CallQueue.java to CallLine.java

---
+++ CallLine.java
@@ -0,0 +1,19 @@
+package SMTravelSimulation;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+/* 
+ * Q.CallLine[3] The Queue entity category with three queues for three types of customers   
+ */ 
+public class CallLine {
+
+	// ZIBO MENG
+	 static Queue[] qCallLine = new Queue[3];
+	//	static Queue<Call> qCallLine = new LinkedList<>();
+
+	// no need to set is as a class actally
+	// put in into the class:SMTravle
+		
+	}
+
+
