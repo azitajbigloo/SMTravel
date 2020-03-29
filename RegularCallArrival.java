75556041ee7650c4b95cbacf17183d4c442031ad
azitajbigloo <60991637+azitajbigloo@users.noreply.github.com>
2020-03-29 02:15:36 -0400

    Update RegularCallArrival.java

--- RegularCallArrival.java
+++ RegularCallArrival.java
@@ -1,37 +1,40 @@
 package SMTravelSimulation;
 import simulationModelling.ScheduledAction;
+/*
+ * Scheduled Action RegularCallArrival 
+ */
 public class RegularCallArrival extends ScheduledAction{
 	
-	    SMTravel model; 
-
+	    SMTravel model;  // Access to the SMTravel model
+	    
+	    // Constructor 
 	    RegularCallArrival(SMTravel model) { this.model = model; }
 
 	    @Override
-	    public double timeSequence()
-	    {
+	    public double timeSequence(){
 	        return(model.rvp.duCallReg());
 	    }
 
 	    @Override
 	    protected void actionEvent() {
-	      
 			// Arrival Action Sequence SCS
 	    	Call icCall = model.spDerive(icCall);
 	    	// Call icCall = new Call();
-	    	icCall.uCuType = Constants.REGULAR;
+	    	icCall.uCuType = Constants.REGULAR;   // Set Customer type to REGULAR
 	    	icCall.uCaType = model.rvp.uCaType();	 
 	    	model.udp.UpdateNumArrivalsOtput(icCall.uCuType);
-	    	if(TrunkLine.n < (Trunkline.numLines - TrunkLine.numReserved)) {
-	    		model.spInsertGrp( rgTrunkLine, icCall);
+	    	//If there is an available trunkline add the Call
+	    	if(model.rgTrunkLine.n < (model.rgTrunkLine.numLines - model.rgTrunkLine.numReserved)) {
+	    		model.spInsertGrp(model.rgTrunkLine, icCall);
 	    		//model.rgTrunkLine.add(icCall);
-	    		model.spStartSequel(EstimateWaitTime(icCall)); // deleted icCall from the parameters, but it should be in the CM
-	    		
+	    		EstimateWaitTime estWTime = new EstimateWaitTime(model, icCall); // Initiate next event
+				model.spStartSequel(estWTime); 
 	    	}
 	    	else {
-	    		model.udp.UpdateNumBusySOutput(icCall.uCuType);
+	    		// Call receives busy signal and leaves.
+	    		model.udp.UpdateNumBusyOutput(icCall.uCuType); 
 	    		model.spLeave(icCall);
 	    	}
 	    }
-
-	}	
+}	
 
