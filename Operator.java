9940f5a04f4409995b6bfcea2b2da0ba451c4eba
azitajbigloo <60991637+azitajbigloo@users.noreply.github.com>
2020-03-28 23:57:05 -0400

    Update Operator.java

--- Operator.java
+++ Operator.java
@@ -1,19 +1,27 @@
-package smTravel;
-
+package SMTravelSimulation;
 import java.util.ArrayList;
 import java.util.HashSet;
-
+/* 
+ *  RG.Operator[3] Entity Category    
+ */
 public class Operator {
 	
-	protected enum operatorType {REGULAR, SILVER, GOLD}; //type of operators
-	protected operatorType uType;
+		int[] uShift; 	//The number of operators starting at each shift time
+		Operator(int[] uShift){
+			this.uShift = uShift; 
+		}
+	/** shouldn't this be in Constants class??*/
+		protected enum operatorType {REGULAR, SILVER, GOLD};   //Type of operators
+		protected operatorType uType;
 	
-	private int uShift; //indicates the number of operators starting at each shift time
-	private ArrayList<Operator> list = new ArrayList<Operator>();
-	private int n; //The number of entities in the list (maximum values is numOperators)
-	private int numOperator; //total number of operators within a day
-	private int numBusy; //The assigned value represents the number of operators that are busy
-	
-	
+		//private int uShift; 	//The number of operators starting at each shift time
+		ArrayList<Operator> list = new ArrayList<Operator>();  	// List of operator objects that can service a call
+		int n; 	//The number of entities in the list (maximum values is uNumOperators)
+		int uNumOperator; 	//The total number of operators within a day
+		int numBusy; 	//The assigned value represents the number of operators that are busy
+
+		
+		
+
+		    
 }
-	
