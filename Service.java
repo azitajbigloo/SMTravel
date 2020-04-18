bb1c898a4c1c29e041a0fba6a9e9144c5230e8cb
azitajbigloo <60991637+azitajbigloo@users.noreply.github.com>
2020-04-17 22:45:28 -0400

    Update Service.java

--- Service.java
+++ Service.java
@@ -1,41 +1,53 @@
-
-package simModel;
+package SMTravelSimulation;
+import java.awt.List;
 
 import simulationModelling.ConditionalActivity;
+/*
+ * Conditional Activity Service 
+ */
+public class Service extends ConditionalActivity{
 
-public class Service extends ConditionalActivity
-{
-   SMTravel model;
-   private Call icCall;
-   private int callType;
-   private int operatorType;
-   
-   //Constructor
-   public Service(SMTravel model, int callType, int operatorType){
-     this.model = model;
-     this.callType = callType;
-     this.operatorType = operatorType;
-   }
-   
-   protected static boolean precondition(SMTravel model)
-   {
-      boolean returnValue = false;
-      if(model.UDP.GetIDsToStartService()!= null)
-         returnValue = true;
-         
-        return(returnValue);
-   }
-   public void startingEvent(){
-      <queueID,operatorID>=model.UDP.GetIDsToStartService();
-      icCall = model.SP.RemoveQue(QCallLine(queueID));
-      model.rgOperators[operatorType].numBusy++;
-   }
-   protected double duration(){
-        return (model.RVP.uSrvTm(operatorType));
-    }
-   protected void terminatingEvent(){
-        model.SP.Leave(icCall);
-        model.SP.StartSequel(AfterCall, operatorType, icCall.uTypeService);
-    }
-   
+	   SMTravel model;
+	   private Call icCall;
+	   private int callType;
+	   private int operatorType;
+	   
+	   //Constructor
+	   public Service(SMTravel model, int callType, int operatorType){
+	     this.model = model;
+	     this.callType = callType;
+	     this.operatorType = operatorType;
+	   }
+	   
+	   protected static boolean precondition(SMTravel model)
+	   {
+	      boolean returnValue = false;
+	      if(GetIDsToStartService()!= null)
+	         returnValue = true;
+	         
+	        return(returnValue);
+	   }
+	   public void startingEvent(){
+	      <queueID,operatorID> = GetIDsToStartService(); //what?
+	      icCall = model.spRemoveQueue(model.qCallLine(queueID)); //what is queueID?
+	      model.rgOperators[operatorType].numBusy++;
+	   }
+	   protected double duration(){
+	        return (model.RVP.uSrvTm(operatorType));
+	    }
+	   protected void terminatingEvent(){
+	        model.SP.Leave(icCall);
+	        model.SP.StartSequel(AfterCall, operatorType, icCall.uTypeService);
+	    }
+	   
+	public List GetIDsToStartService() {
+		int queueID = -1;
+		if( model.qCallLine[Constants.GOLD].n > 0)
+			
+			
+		
+		return null;
+		
+		
+	}
 }
