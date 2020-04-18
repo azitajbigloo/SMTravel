53411c00171a6bd63496371751dade7a9adbe4d7
azitajbigloo <60991637+azitajbigloo@users.noreply.github.com>
2020-04-17 22:47:13 -0400

    Update UDPs.java

--- UDPs.java
+++ UDPs.java
@@ -1,5 +1,5 @@
 package SMTravelSimulation;
-//edited
+
 public class UDPs {
 
 	
@@ -8,30 +8,56 @@ public class UDPs {
 		// Constructor
 		protected UDPs(SMTravel model) { this.model = model; }
 		
-		protected int CanServiceCall(Call icCall) {
+		protected int CanServiceCall(int uCuType) {
 			//icCall.uCuType = this.icCall.uCuType;
 			int operatorID = -1;
-			if ((icCall.uCuType == Constants.REGULAR) && (model.rgOperator[Constants.REGULAR].n < model.rgOperator[Constants.REGULAR].uNumOperator))
+			if ((uCuType.uCuType == Constants.REGULAR) && (model.rgOperator[Constants.REGULAR].n < model.rgOperator[Constants.REGULAR].uNumOperator))
 				 operatorID = Constants.REGULAR;
 			
 			//// azita - edit
 			
-			if (icCall.uCuType == SILVER && rgOperator[SILVER].n < RG.Operator[SILVER].uNumOperators )
+			if (uCuType.uCuType == SILVER && rgOperator[SILVER].n < RG.Operator[SILVER].uNumOperators )
 				return operatorID = SILVER;
-			else if (icCall.uCuType == SILVER && rgOperator[REGULAR].n < rgOperator[REGULAR].uNumOperators)
+			else if (uCuType.uCuType == SILVER && rgOperator[REGULAR].n < rgOperator[REGULAR].uNumOperators)
 				return operatorID == REGULAR;
 			else
 				return null;
 			
-			if (icCall.uCuType == GOLD && rgOperator[GOLD].n < rgOperator[GOLD].uNumOperators)
+			if (uCuType.uCuType == GOLD && rgOperator[GOLD].n < rgOperator[GOLD].uNumOperators)
 				return operatorID = GOLD;	
-			else if (icCall.uCuType = GOLD && rgOperator[SILVER].n < RG.Operator[SILVER].uNumOperators)
+			else if (uCuType.uCuType = GOLD && rgOperator[SILVER].n < RG.Operator[SILVER].uNumOperators)
 				return operatorID = SILVER;
-			else if (icCall.uCuType = GOLD && rgOperator[REGULAR].n < rgOperator[REGULAR].uNumOperators)
+			else if (uCuType.uCuType = GOLD && rgOperator[REGULAR].n < rgOperator[REGULAR].uNumOperators)
 				return operatorID = REGULAR;
 			else 
 				return null;
 		}
 
+		protected void UpdateNumArrivalsOutput(Call icCall) {
 			
+			if(icCall.uCuType ==  Constants.REGULAR) { 
+				model.output.numRegularArrivalCalls ++;
+				model.output.percBusyRegularCalls = model.output.numBusyRegularCalls / model.output.numRegularArrivalCalls;
+			}
+			else {
+				model.output.numCardHArrivalCalls ++;
+				model.output.percBusyCrdHCalls = model.output.numBusyCrdHCalls / model.output.numCardHArrivalCalls;
+			}
+		}
+		
+		protected void UpdateNumBusyOutput(Call icCall) {
+			if(icCall.uCuType ==  Constants.REGULAR) { 
+				model.output.numBusyRegularCalls ++;
+				model.output.percBusyRegularCalls = model.output.numBusyRegularCalls / model.output.numRegularArrivalCalls;
+						
+			}
+			else {
+				model.output.numBusyCrdHCalls ++;
+				model.output.percBusyCrdHCalls = model.output.numBusyCrdHCalls / model.output.numCardHArrivalCalls;
+			}
+		}
 }
+
+
+
+
