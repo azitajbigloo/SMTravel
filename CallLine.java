a173a0fae606a5bc343a00dcc52f57463a067229
azitajbigloo <60991637+azitajbigloo@users.noreply.github.com>
2020-04-17 22:48:04 -0400

    Update CallLine.java

--- CallLine.java
+++ CallLine.java
@@ -1,19 +1,26 @@
 package SMTravelSimulation;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
+//import java.util.Queue;
 /* 
  * Q.CallLine[3] The Queue entity category with three queues for three types of customers   
  */ 
 public class CallLine {
 
-	// ZIBO MENG
-	 static Queue[] qCallLine = new Queue[3];
-	//	static Queue<Call> qCallLine = new LinkedList<>();
+	int n; 		//The number of entities in the list, n
+	// Implement the queue using an ArrayList object
+	private ArrayList<Call> callLine = new ArrayList<Call>();  	// List of call objects waiting to be served (order: FIFO)
+	static CallLine[] qCallLine = new CallLine[3]; // Three Queues for REGULAR, SILVER and GOLD calls
+	//protected enum callLineType {REGULAR, SILVER, GOLD};   //Type of call Lines
+	//protected callLineType uCallLineType;
 
-	// no need to set is as a class actally
-	// put in into the class:SMTravle
-		
+	// getters/setters and standard procedures
+	protected int getN() { return(callLine.size()); }
+	protected void spInsertQue(Call icCall) { callLine.add(icCall); }
+	protected Call spRemoveQue(Call icCall) //parameter??****
+	{ 
+		if(callLine.size() != 0) icCall = callLine.remove(0);
+		return(icCall);
+	}
 	}
 
 
