ceefed503256f32d7f932eb48757837a47b03c86
azitajbigloo <60991637+azitajbigloo@users.noreply.github.com>
2020-04-17 22:46:51 -0400

    Update RegularCallArrival.java

--- RegularCallArrival.java
+++ RegularCallArrival.java
@@ -12,18 +12,19 @@ public class RegularCallArrival extends ScheduledAction{
 
 	    @Override
 	    public double timeSequence(){
-	        return(model.rvp.duCallReg());
+	        return(model.rvp.DuCallReg());
 	    }
 
 	    @Override
 	    protected void actionEvent() {
 			// Arrival Action Sequence SCS
-	    	Call icCall = model.spDerive(icCall);
+	    	Call icCall = null; //    ?????
+	    	icCall = model.spDerive(icCall); // ???
 	    	// Call icCall = new Call();
 	    	icCall.uCuType = Constants.REGULAR;   // Set Customer type to REGULAR
 	    	icCall.uCaType = model.rvp.uCaType();	 
-	    	model.udp.UpdateNumArrivalsOtput(icCall.uCuType);
-	    	//If there is an available trunkline add the Call
+	    	model.udp.UpdateNumArrivalsOutput(icCall);
+	    	//If there is an available trunk line add the Call
 	    	if(model.rgTrunkLine.n < (model.rgTrunkLine.numLines - model.rgTrunkLine.numReserved)) {
 	    		model.spInsertGrp(model.rgTrunkLine, icCall);
 	    		//model.rgTrunkLine.add(icCall);
@@ -32,7 +33,7 @@ public class RegularCallArrival extends ScheduledAction{
 	    	}
 	    	else {
 	    		// Call receives busy signal and leaves.
-	    		model.udp.UpdateNumBusyOutput(icCall.uCuType); 
+	    		model.udp.UpdateNumBusyOutput(icCall); 
 	    		model.spLeave(icCall);
 	    	}
 	    }
