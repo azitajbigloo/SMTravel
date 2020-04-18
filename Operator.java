917d80e4d57e50acd4270bf0ded79b1f7c6c6d44
azitajbigloo <60991637+azitajbigloo@users.noreply.github.com>
2020-04-17 22:47:39 -0400

    Update Operator.java

--- Operator.java
+++ Operator.java
@@ -6,22 +6,29 @@ import java.util.HashSet;
  */
 public class Operator {
 	
-		int[] uShift; 	//The number of operators starting at each shift time
-		Operator(int[] uShift){
-			this.uShift = uShift; 
-		}
-	/** shouldn't this be in Constants class??*/
-		protected enum operatorType {REGULAR, SILVER, GOLD};   //Type of operators
-		protected operatorType uType;
+	// Attributes
+	int n; 	//The number of entities in the list, n (maximum values is uNumOperators)
+	//ArrayList<Operator> list = new ArrayList<Operator>();  	// List of operator objects that can service a call
+	// For implementing the group, use a HashSet object.
+	protected HashSet<Operator> group = new HashSet<Operator>(); // RG.Operator[].list
+	int uNumOperators; 	//The total number of operators within a day
+	int numBusy; 	//The assigned value represents the number of operators that are busy
+
+	int[] uShift; 	//The number of operators starting at each shift
+	Operator(int[] uShift){
+		this.uShift = uShift; 
+	}
+	protected enum operatorType {REGULAR, SILVER, GOLD};   //Type of operators
+	protected operatorType uOperatorType;
+	int operatorID; // *** what is the use???? **/////
 	
-		//private int uShift; 	//The number of operators starting at each shift time
-		ArrayList<Operator> list = new ArrayList<Operator>();  	// List of operator objects that can service a call
-		int n; 	//The number of entities in the list (maximum values is uNumOperators)
-		int uNumOperator; 	//The total number of operators within a day
-		int numBusy; 	//The assigned value represents the number of operators that are busy
-
-		
-		
-
-		    
+	protected int getN() { return group.size(); }  // Attribute n
+	
+	// are these needed?
+	/******
+	// Required methods to manipulate the group, standard procedures
+	protected void insertGrp(Operator icCall) {	group.add(icCall); }
+	protected boolean removeGrp(Call icCall) { return(group.remove(icCall)); }
+	
+	*****//////
 }
