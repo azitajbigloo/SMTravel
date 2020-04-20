9432bf1b95e578291422db23df7df0b16a29a8bb
Hang(LEO) Gong <47484740+hgong11@users.noreply.github.com>
2020-04-20 16:05:12 -0400

    CallId.OperatorId to get
    Hang Gong updated for udp used for service

--- UDPs.java
+++ UDPs.java
@@ -32,6 +32,50 @@ public class UDPs {
 			else 
 				return null;
 		}
+	        //Hang Gong edited for Get OperatorId
+		protected int GetOperatorToServe(){
+			int operatorId=-1;
+			 for (int opType = 2; opType >= 0; opType--) { //Prioritize GOLD operators
+		            if(model.rgOperator[opType].n < model.rgOperator[opType].uNumOperators) {
+		                if (!model.qCallLine.get(Constants.GOLD).isEmpty()){
+		                    operatorId = opType;
+		                    break;
+		                } else if ((opType == Constants.REGULAR || opType == Constants.SILVER)
+		                        && !model.qCallLine.get(Constants.SILVER).isEmpty()) {
+		                    operatorId = opType;
+		                    break;
+		                } else if (opType == Constants.REGULAR
+		                        && !model.qCallLine.get(Constants.REGULAR).isEmpty()) {
+		                    operatorId = opType;
+		                    break;
+		                }
+		            }
+		        }
+			 return operatorId;
+			
+		}
+		
+		// Hang Gong edited for Get CallId//
+		protected int GetCallToServe(){
+			int CallId=-1;
+			 for (int caType = 2; caType >= 0; caType--) { //Prioritize GOLD operators
+		            if(model.rgOperator[caType].n < model.rgOperator[caType].uNumOperators) {
+		                if (!model.qCallLine.get(Constants.GOLD).isEmpty()){
+		                    CallId = caType;
+		                    break;
+		                } else if ((caType == Constants.REGULAR || caType == Constants.SILVER)
+		                        && !model.qCallLine.get(Constants.SILVER).isEmpty()) {
+		                    CallId = caType;
+		                    break;
+		                } else if (caType == Constants.REGULAR
+		                        && !model.qCallLine.get(Constants.REGULAR).isEmpty()) {
+		                    CallId = caType;
+		                    break;
+		                }
+		            }
+		        }
+			 return CallId;
+		}
                 //Hang Gong edited for UPdateWaitCallsOutput Call for service//
 		protected void UpdateWaitCallsOutput(Call icCall){
 			 icCall.waitTime = model.getClock() - icCall.startWaitTime;
