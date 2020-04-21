85d1f4d6909d28cb345af68027722bb8f67b3e99
azitajbigloo <60991637+azitajbigloo@users.noreply.github.com>
2020-04-21 14:18:54 -0400

    Update Seeds.java

--- Seeds.java
+++ Seeds.java
@@ -1,32 +1,30 @@
 package SMTravelSimulation;
-
 import cern.jet.random.engine.RandomSeedGenerator;
+public class Seeds {
 
-public class Seeds
-{
-    int[] arrRegular;
-    int[] arrCardholder;
-    int cardholderType;
-    int[] serviceTime;
-    int[] afterCallTime;
-    int[] toleratedWaitTime;
-    int typingTime;
-    int callSubject;
+	    int[] arrRegular;
+	    int[] arrCardholder;
+	    int cardholderType;
+	    int[] serviceTime;
+	    int[] afterCallTime;
+	    int[] toleratedWaitTime;
+	    int typingTime;
+	    int callType;
 
 
-    public Seeds(RandomSeedGenerator rsg)
-    {
-        arrRegular = new int[12];
-        arrCardholder = new int[12];
-        for (int i = 0; i < 12; i++){
-            arrRegular[i] = rsg.nextSeed();
-            arrCardholder[i] = rsg.nextSeed();
-        }
-        cardholderType = rsg.nextSeed();
-        serviceTime = new int[]{rsg.nextSeed(), rsg.nextSeed(), rsg.nextSeed()};
-        afterCallTime = new int[]{rsg.nextSeed(), rsg.nextSeed(), rsg.nextSeed()};
-        toleratedWaitTime = new int[]{rsg.nextSeed(), rsg.nextSeed()};
-        typingTime = rsg.nextSeed();
-        callSubject = rsg.nextSeed();
-    }
+	    public Seeds(RandomSeedGenerator rsg)
+	    {
+	        arrRegular = new int[12];
+	        arrCardholder = new int[12];
+	        for (int i = 0; i < 12; i++){
+	            arrRegular[i] = rsg.nextSeed();
+	            arrCardholder[i] = rsg.nextSeed();
+	        }
+	        cardholderType = rsg.nextSeed();
+	        serviceTime = new int[]{rsg.nextSeed(), rsg.nextSeed(), rsg.nextSeed()};
+	        afterCallTime = new int[]{rsg.nextSeed(), rsg.nextSeed(), rsg.nextSeed()};
+	        toleratedWaitTime = new int[]{rsg.nextSeed(), rsg.nextSeed()};
+	        typingTime = rsg.nextSeed();
+	        callType = rsg.nextSeed();
+	    }
 }
