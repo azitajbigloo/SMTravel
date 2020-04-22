7efbeba2a9d80b7760d867b209e43965f65d1b9a
azitajbigloo <60991637+azitajbigloo@users.noreply.github.com>
2020-04-21 20:15:35 -0400

    Update Experiment.java

--- Experiment.java
+++ Experiment.java
@@ -1,18 +1,17 @@
-// File: Experiment.java
-// Description:
 
 import SMTravelSimulation.SMTravel;
+import SMTravelSimulation.Seeds;
 import cern.jet.random.engine.*;
 
 // Main Method: Experiments
 // 
 class Experiment
 {
-   static int [] [] schedule =
+   static int [] [] schedules =
    {
-	{ ？, ？, ？, ？}, 
-	{ ？, ？, ？, ？}, 
-	{ ？, ？, ？, ？},    
+		   { 10, 0, 0, 0, 10},  // Case 1 (Base case)
+		   { 10, 0, 0, 0, 10},  // Case 3
+		   { 5, 0, 0, 0, 5}  // Case 4		   
    };
    public static void main(String[] args)
    {
@@ -30,13 +29,13 @@ class Experiment
        System.out.println(" Case 1");
        for(i=0 ; i < NUMRUNS ; i++)
        {
-          SMT1 = new SMTravel(startTime,endTime,schedule,55,15,sds[i],ture);
+          SMT1 = new SMTravel(startTime,endTime,schedules,55,15,sds[i],true);
           SMT1.runSimulation();
-          System.out.println("Perc900SecRegularCalls:"+SMT1.getPerc900SecRegularCalls())
-          System.out.println("Perc180SecSilverCalls:"+SMT1.getPerc180SecSilverCalls())
-          System.out.println("Perc90SecGoldCalls:"+SMT1.getPerc90SecGoldCalls())
-          System.out.println("PercBusyCrdHCalls:"+SMT1.getPercBusyCrdHCalls())
-          System.out.println("PercBusyRegularCalls:"+SMT1.getPercBusyRegularCalls())
+          System.out.println("Perc900SecRegularCalls:"+SMT1.getPerc900SecRegularCalls());
+          System.out.println("Perc180SecSilverCalls:"+SMT1.getPerc180SecSilverCalls());
+          System.out.println("Perc90SecGoldCalls:"+SMT1.getPerc90SecGoldCalls());
+          System.out.println("PercBusyCrdHCalls:"+SMT1.getPercBusyCrdHCalls());
+          System.out.println("PercBusyRegularCalls:"+SMT1.getPercBusyRegularCalls());
           // See examples for hints on collecting output
           // and developping code for analysis
        }
