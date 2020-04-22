bc9a0eae2db722788622bb1912d022b2ea483425
azitajbigloo <60991637+azitajbigloo@users.noreply.github.com>
2020-04-21 20:18:45 -0400

    Update Service.java

--- Service.java
+++ Service.java
@@ -1,5 +1,7 @@
+
 package SMTravelSimulation;
-import java.util.ArrayList;
+
+
 import simulationModelling.ConditionalActivity;
 /*
  * Conditional Activity Service 
@@ -12,17 +14,10 @@ public class Service extends ConditionalActivity{
 	   private int callType;
 	   
 	   //Constructor
-	   public Service(SMTravel model, int operatorType, int callType){
+	   public Service(SMTravel model){
 	     this.model = model;
-	     this.operatorType = operatorType;
-	     this.callType = callType;
 	   }
-	   // added for the initialise action and activities in SMTravel Class
-	   public Service(SMTravel model) {
-		// TODO Auto-generated constructor stub
-		   this.model = model;
-	}
-	// IF THE CanService return True the service start.
+	   // IF THE CanService return True the service start.
 	   protected static boolean precondition(SMTravel model)
 	   {
 	      boolean returnValue = false;
@@ -36,16 +31,14 @@ public class Service extends ConditionalActivity{
 		  operatorType = model.udp.GetOperatorToServe();
 		  callType = model.udp.GetCallToServe();
 		  icCall = model.qCallLine.get(callType).remove(0);
-	     // model.rgOperator[operatorType].insertGrp(icCall);
 	      model.rgOperator[operatorType].numBusy++;
 	   }
 	   protected double duration(){
-	        return (model.rvp.uSrvTm(icCall.uCaType, operatorType));//Call attribute need to change based on the type
+	        return (model.rvp.uSrvTm(icCall.uType,operatorType));//Call attrubute need to change based on the type
 	    }
 	   protected void terminatingEvent(){
-	        model.rgTrunkLine.removeGrp(icCall);
-
-	        //model.rgTrunkLine.numTrunkLineInUse--; no attribute for this in use trunk lines.//
+	        model.rgTrunkLine.removeList(icCall);
+	        //model.rgTrunkLine.numTrunkLineInUse--; no attribute for this in use trunklines.//
 	        AfterCall afterCall = new AfterCall(model, icCall, operatorType);//
 	        model.spStart(afterCall);
 	   }}
